dabd8c6d3f46a1c6aebc021da704f1c50f444edb
MitasMax90 <Rostishka90@mail.ru>
2022-01-03 23:52:07 +0300

    Second commit

---
+++ src/Machine.java
@@ -0,0 +1,30 @@
+public class Machine {
+    private int oil;
+    private boolean isOff;
+    private String name;
+
+    void makeDetails(int amount) {
+        if (oil >= amount) {
+            oil -= amount;
+            System.out.println(name + ": Work is donexfgnhjsrth!");
+        } else if (oil < amount) {
+            System.out.println(name + ": We are need more oil");
+            isOff = true;
+        }
+    }
+
+    void refuel(int refOil) {
+        oil += refOil;
+        System.out.println(name + ": Is ready to work!");
+        isOff = false;
+    }
+
+    void start() {
+        this.isOff = false;
+    }
+
+    public Machine(int oil, String name) {
+        this.oil = oil;
+        this.name = name;
+    }
+}
