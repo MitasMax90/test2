b6a648a160e01eea6554be388512bcf49f6061dc
MitasMax90 <Rostishka90@mail.ru>
2022-01-04 13:59:09 +0300

    var1

--- src/Machine.java
+++ src/Machine.java
@@ -1,21 +1,21 @@
 public class Machine {
     private int oil;
     private boolean isOff;
-    private String name;
+    private String name2;
 
     void makeDetails(int amount) {
         if (oil >= amount) {
             oil -= amount;
-            System.out.println(name + ": Work is done!");
+            System.out.println(name2 + ": Work is done!");
         } else if (oil < amount) {
-            System.out.println(name + ": We are need more oil");
+            System.out.println(name2 + ": We are need more oil");
             isOff = true;
         }
     }
 
     void refuel(int refOil) {
         oil += refOil;
-        System.out.println(name + ": Is ready to work!");
+        System.out.println(name2 + ": Is ready to work!");
         isOff = false;
     }
 
@@ -25,6 +25,6 @@ public class Machine {
 
     public Machine(int oil, String name) {
         this.oil = oil;
-        this.name = name;
+        this.name2 = name;
     }
 }
